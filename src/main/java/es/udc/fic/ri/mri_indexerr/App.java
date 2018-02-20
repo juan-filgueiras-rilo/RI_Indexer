76d36c05521f71d400ba1747f8ee16acbe7955c8
Juan Luis Filgueiras Rilo <juan.filgueiras.rilo@udc.es>
2018-02-20 18:37:02 +0100

    commit inicial

---
+++ src/main/java/es/udc/fic/ri/mri_indexerr/App.java
@@ -0,0 +1,13 @@
+package es.udc.fic.ri.mri_indexerr;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
