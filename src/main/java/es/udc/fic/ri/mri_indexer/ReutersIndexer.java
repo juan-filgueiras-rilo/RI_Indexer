be109b40a7d705c8e112f703d8ae2de6be588b38
Juan Luis Filgueiras Rilo <juan.filgueiras.rilo@udc.es>
2018-03-06 17:53:34 +0100

    campos indexados

--- src/main/java/es/udc/fic/ri/mri_indexer/ReutersIndexer.java
+++ src/main/java/es/udc/fic/ri/mri_indexer/ReutersIndexer.java
@@ -11,6 +11,8 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.List;
@@ -18,6 +20,8 @@ import java.util.List;
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.LongPoint;
+import org.apache.lucene.document.DateTools;
+import org.apache.lucene.document.DateTools.Resolution;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
 import org.apache.lucene.document.StringField;
@@ -30,26 +34,38 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 
 public class ReutersIndexer {
-	private ReutersIndexer() {
-	}
+	
+	private ReutersIndexer() {}
 
 	/** Index all text files under a directory. */
 	public static void main(String[] args) {
 		String usage = "java org.apache.lucene.demo.IndexFiles" + " [-index INDEX_PATH] [-docs DOCS_PATH] [-update]\n\n"
 				+ "This indexes the documents in DOCS_PATH, creating a Lucene index"
 				+ "in INDEX_PATH that can be searched with SearchFiles";
-		String indexPath = "index";
-		String docsPath = null;
-		boolean create = true;
-		for (int i = 0; i < args.length; i++) {
+		//String indexPath = "";
+		String indexPath = "D:\\UNI\\3º\\Recuperación de la Información\\2018";
+		//String docsPath = "";
+		String docsPath = "D:\\UNI\\3º\\Recuperación de la Información\\Práctica 2\\reuters21578";
+		byte modo = 0; // 0 create ; 1 append; 2 createorappend
+		for(int i=0;i<args.length;i++) {
 			if ("-index".equals(args[i])) {
-				indexPath = args[i + 1];
+				indexPath = args[i+1];
 				i++;
-			} else if ("-docs".equals(args[i])) {
-				docsPath = args[i + 1];
+			} else if ("-coll".equals(args[i])) {
+				docsPath = args[i+1];
 				i++;
-			} else if ("-update".equals(args[i])) {
-				create = false;
+			} else if ("-openmode".equals(args[i])) {
+				switch(args[i+1]) {
+					case "create": modo=0;
+						break;
+					case "append": modo=1;
+						break;
+					case "create_or_append": modo=2;
+						break;
+					default: System.err.println("Wrong option -openmode. " + usage);
+						System.exit(1);
+				}
+				i++;
 			}
 		}
 
@@ -60,8 +76,7 @@ public class ReutersIndexer {
 
 		final Path docDir = Paths.get(docsPath);
 		if (!Files.isReadable(docDir)) {
-			System.out.println("Document directory '" + docDir.toAbsolutePath()
-					+ "' does not exist or is not readable, please check the path");
+			System.out.println("Document directory '" +docDir.toAbsolutePath()+ "' does not exist or is not readable, please check the path");
 			System.exit(1);
 		}
 
@@ -73,32 +88,27 @@ public class ReutersIndexer {
 			Analyzer analyzer = new StandardAnalyzer();
 			IndexWriterConfig iwc = new IndexWriterConfig(analyzer);
 
-			if (create) {
-				// Create a new index in the directory, removing any
-				// previously indexed documents:
-				iwc.setOpenMode(OpenMode.CREATE);
-			} else {
-				// Add new documents to an existing index:
-				iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+			switch (modo) {
+				case 0: iwc.setOpenMode(OpenMode.CREATE);
+						break;
+				case 1: iwc.setOpenMode(OpenMode.APPEND);
+						break;
+				case 2: iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+						break;
 			}
 
-			// Optional: for better indexing performance, if you
-			// are indexing many documents, increase the RAM
-			// buffer. But if you do this, increase the max heap
-			// size to the JVM (eg add -Xmx512m or -Xmx1g):
-			//
-			// iwc.setRAMBufferSizeMB(256.0);
+			iwc.setRAMBufferSizeMB(512.0);
 
 			IndexWriter writer = new IndexWriter(dir, iwc);
 			indexDocs(writer, docDir);
 
 			// NOTE: if you want to maximize search performance,
-			// you can optionally call forceMerge here. This can be
+			// you can optionally call forceMerge here.  This can be
 			// a terribly costly operation, so generally it's only
 			// worth it when your index is relatively static (ie
 			// you're done adding documents to it):
 			//
-			// writer.forceMerge(1);
+			writer.forceMerge(1);
 
 			writer.close();
 
@@ -106,7 +116,8 @@ public class ReutersIndexer {
 			System.out.println(end.getTime() - start.getTime() + " total milliseconds");
 
 		} catch (IOException e) {
-			System.out.println(" caught a " + e.getClass() + "\n with message: " + e.getMessage());
+			System.out.println(" caught a " + e.getClass() +
+					"\n with message: " + e.getMessage());
 		}
 	}
 
@@ -114,20 +125,16 @@ public class ReutersIndexer {
 	 * Indexes the given file using the given writer, or if a directory is given,
 	 * recurses over files and directories found under the given directory.
 	 * 
-	 * NOTE: This method indexes one document per input file. This is slow. For good
-	 * throughput, put multiple documents into your input file(s). An example of
-	 * this is in the benchmark module, which can create "line doc" files, one
-	 * document per line, using the <a href=
-	 * "../../../../../contrib-benchmark/org/apache/lucene/benchmark/byTask/tasks/WriteLineDocTask.html"
+	 * NOTE: This method indexes one document per input file.  This is slow.  For good
+	 * throughput, put multiple documents into your input file(s).  An example of this is
+	 * in the benchmark module, which can create "line doc" files, one document per line,
+	 * using the
+	 * <a href="../../../../../contrib-benchmark/org/apache/lucene/benchmark/byTask/tasks/WriteLineDocTask.html"
 	 * >WriteLineDocTask</a>.
-	 * 
-	 * @param writer
-	 *            Writer to the index where the given file/dir info will be stored
-	 * @param path
-	 *            The file to index, or the directory to recurse into to find files
-	 *            to index
-	 * @throws IOException
-	 *             If there is a low-level I/O error
+	 *  
+	 * @param writer Writer to the index where the given file/dir info will be stored
+	 * @param path The file to index, or the directory to recurse into to find files to index
+	 * @throws IOException If there is a low-level I/O error
 	 */
 	static void indexDocs(final IndexWriter writer, Path path) throws IOException {
 		if (Files.isDirectory(path)) {
@@ -180,25 +187,34 @@ public class ReutersIndexer {
 			for (Iterator<List<String>> iterator = parsedContent.iterator(); iterator.hasNext();) {
 
 				List<String> parsedDoc = iterator.next();
-				Field pathField = new StringField("path", file.toString(), Field.Store.YES);
-				doc.add(pathField);
-
-				// Add the last modified date of the file a field named "modified".
-				// Use a LongPoint that is indexed (i.e. efficiently filterable with
-				// PointRangeQuery). This indexes to milli-second resolution, which
-				// is often too fine. You could instead create a number based on
-				// year/month/day/hour/minutes/seconds, down the resolution you require.
-				// For example the long value 2011021714 would mean
-				// February 17, 2011, 2-3 PM.
-				doc.add(new LongPoint("modified", lastModified));
-
-				// Add the contents of the file to a field named "contents". Specify a Reader,
-				// so that the text of the file is tokenized and indexed, but not stored.
-				// Note that FileReader expects the file to be in UTF-8 encoding.
-				// If that's not the case searching for special characters will fail.
-				doc.add(new TextField("contents",
-						new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8))));
-
+				
+				Field pathSgm = new StringField("path", file.toString(), Field.Store.YES);
+				doc.add(pathSgm);
+				
+				Field hostname = new StringField("hostname", System.getProperty("user.name"), Field.Store.YES);
+				doc.add(hostname);
+				
+				//Thread
+				
+				Field topics = new TextField("topics", parsedDoc.get(2), Field.Store.YES);
+				doc.add(topics);
+				
+				Field title = new TextField("title", parsedDoc.get(0), Field.Store.YES);
+				doc.add(title);
+				
+				Field dateLine = new StringField("dateline", parsedDoc.get(4), Field.Store.YES);
+				doc.add(dateLine);
+				
+				Field body = new TextField("body", parsedDoc.get(1), Field.Store.YES);
+				doc.add(body);
+				
+				//26-FEB-1987 15:01:01.79
+				SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MMMM-yyyy HH:mm:ss.SS");
+				Date date = dateFormat.parse(parsedDoc.get(3));
+				String dateText = DateTools.dateToString(date, Resolution.SECOND);
+				Field dateField = new StringField("date", dateText, Field.Store.YES);
+				doc.add(dateField);
+				
 				if (writer.getConfig().getOpenMode() == OpenMode.CREATE) {
 					// New index, so we just add the document (no old document can be there):
 					System.out.println("adding " + file);
@@ -211,6 +227,9 @@ public class ReutersIndexer {
 					writer.updateDocument(new Term("path", file.toString()), doc);
 				}
 			}
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 }
