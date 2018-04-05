fb2779e4c16978916da1ceb08288e5ca334ddad5
TitoPazo <4b18ivan@gmail.com>
2018-04-05 21:57:16 +0200

    fallo mio

---
+++ src/main/java/es/udc/fic/ri/mri_indexer/SentenceTokenizer.java
@@ -0,0 +1,19 @@
+package es.udc.fic.ri.mri_indexer;
+
+public class SentenceTokenizer {
+
+    private String text;
+
+    public SentenceTokenizer()  {
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String[] getSentences() {
+        return text.split("\\.");
+    }
+
+}
+
